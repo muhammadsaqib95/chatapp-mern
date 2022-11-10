9268f6af40d743ee4f4dba0ad98753f3d51a5387
saqib <saqib.idrees95@gmail.com>
2022-11-10 21:16:06 +0500

    minor changes

--- frontend/src/components/utility.jsx
+++ frontend/src/components/utility.jsx
@@ -1,35 +1,35 @@
 export function debounce(cb, delay = 1000) {
-    let timeout
-    return (...args) => {
-      clearTimeout(timeout)
-      timeout = setTimeout(() => {
-        cb(...args)
-      }, delay)
-    }
-  }
-  
+  let timeout;
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      cb(...args);
+    }, delay);
+  };
+}
+
 export function throttle(cb, delay = 1000, immediate = false) {
-    let shouldWait = false
-    let waitingArgs
-    const timeoutFunc = () => {
-      if (waitingArgs == null) {
-        shouldWait = false
-      } else {
-        cb(...waitingArgs)
-        waitingArgs = null
-        setTimeout(timeoutFunc, delay)
-      }
+  let shouldWait = false;
+  let waitingArgs;
+  const timeoutFunc = () => {
+    if (waitingArgs == null) {
+      shouldWait = false;
+    } else {
+      cb(...waitingArgs);
+      waitingArgs = null;
+      setTimeout(timeoutFunc, delay);
     }
-  
-    return (...args) => {
-      if (shouldWait) {
-        waitingArgs = args
-        return
-      }
-  
-      cb(...args)
-      shouldWait = true
-  
-      setTimeout(timeoutFunc, delay)
+  };
+
+  return (...args) => {
+    if (shouldWait) {
+      waitingArgs = args;
+      return;
     }
-  }
+
+    cb(...args);
+    shouldWait = true;
+
+    setTimeout(timeoutFunc, delay);
+  };
+}
